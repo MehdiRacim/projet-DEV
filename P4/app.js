2e4bb55bd3d1cee370010259769a5d714c643ab8
djilali yahia <DYAHIABERROUIGUET>
2024-05-29 01:47:32 +0200

    caAvanceSurLeProject

--- P4/app.js
+++ P4/app.js
@@ -24,39 +24,39 @@ function checkWin(board, row, col, player) {
 
 // Fonction pour mettre à jour l'état du jeu et vérifier si quelqu'un a gagné
 function updateGameState(room, move, player) {
-  const gameState = gameStates[room];
-  const column = move;
-  const row = gameState.heights[column];
-  
-  // Vérifier si la colonne est pleine
-  if (row >= 6) {
-      return;
-  }
-  
-  // Mettre à jour le plateau de jeu avec le mouvement du joueur
-  gameState.board[row][column] = player;
-  gameState.heights[column]++;
-  
-  // Vérifier s'il y a un gagnant
-  gameState.winner = checkWin(gameState.board, row, column, player);
-  
-  // Passer au prochain joueur
-  gameState.currentPlayer = gameState.currentPlayer === player1 ? player2 : player1;
+    const gameState = gameStates[room];
+    const column = move;
+    const row = gameState.heights[column];
+
+    // Vérifier si la colonne est pleine
+    if (row >= 6) {
+        return;
+    }
+
+    // Mettre à jour le plateau de jeu avec le mouvement du joueur
+    gameState.board[row][column] = player;
+    gameState.heights[column]++;
+
+    // Vérifier s'il y a un gagnant
+    gameState.winner = checkWin(gameState.board, row, column, player);
+
+    // Passer au prochain joueur
+    gameState.currentPlayer = gameState.currentPlayer === gameState.player1 ? gameState.player2 : gameState.player1;
 }
 
 // Route pour servir la page d'accueil
 app.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname, 'index.html'));
+    res.sendFile(path.join(__dirname, 'index.html'));
 });
 
 // Route pour servir la page d'attente
 app.get('/wait.html', (req, res) => {
-  res.sendFile(path.join(__dirname, 'wait.html'));
+    res.sendFile(path.join(__dirname, 'wait.html'));
 });
 
 // Route pour servir la page Puissance 4
 app.get('/puissance4.html', (req, res) => {
-  res.sendFile(path.join(__dirname, 'puissance4.html'));
+    res.sendFile(path.join(__dirname, 'puissance4.html'));
 });
 
 // Socket.IO logic
@@ -64,13 +64,13 @@ io.on('connection', (socket) => {
     console.log('Nouveau client connecté', socket.id);
 
     // Ajouter le joueur à la file d'attente
-    waitingPlayers.push(socket.id);
-    console.log(`Joueurs en attente: ${waitingPlayers}`);
+    waitingPlayers.push({ id: socket.id });
+    console.log(`Joueurs en attente: ${waitingPlayers.map(player => player.id)}`);
 
     // Vérifier si nous avons au moins deux joueurs en attente
     if (waitingPlayers.length >= 2) {
-        const player1 = waitingPlayers.shift();
-        const player2 = waitingPlayers.shift();
+        const player1 = waitingPlayers.shift().id;
+        const player2 = waitingPlayers.shift().id;
         const room = `game_${player1}_${player2}`;
         console.log(`Match trouvé : ${player1} vs ${player2} dans la salle ${room}`);
 
@@ -87,6 +87,8 @@ io.on('connection', (socket) => {
             board: Array.from({ length: 6 }, () => Array.from({ length: 7 }, () => null)),
             heights: Array(7).fill(0),
             winner: null,
+            player1: player1,
+            player2: player2,
             currentPlayer: player1 // Joueur 1 commence
         };
 
@@ -95,23 +97,24 @@ io.on('connection', (socket) => {
     }
 
     socket.on('make_move', (data) => {
+      
         const room = data.room;
         const move = data.move;
         const player = socket.id;
         const gameState = gameStates[room];
-        if (!gameState || gameState.winner || player !== gameState.currentPlayer) {
-            return;
-        }
+        console.log(`move`);//llllllllllleeeeeeeeeeeProblemmeeeeeeeeeeeeestlaaaaaaaaaaaaa
+        
         updateGameState(room, move, player);
         io.to(room).emit('game_state', gameState);
+        
     });
 
     socket.on('disconnect', () => {
         console.log('Client déconnecté', socket.id);
-        waitingPlayers = waitingPlayers.filter(player => player !== socket.id);
+        waitingPlayers = waitingPlayers.filter(player => player.id !== socket.id);
     });
 });
 
 server.listen(port, () => {
-  console.log(`Serveur écoutant sur le port ${port}`);
+    console.log(`Serveur écoutant sur le port ${port}`);
 });
